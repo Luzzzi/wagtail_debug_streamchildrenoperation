55d3f5069f06e75c201b4ed73b07df77c941ad52
Lucie Le Frapper <lucielefrapper@macbook-pro-de-lucie.home>
2023-07-05 11:18:04 +0200

    initial commit

---
+++ wagtail_debug_streamchildrenoperation/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-&4vh!p761fh2i%5h0%221h*^d$9t4)-pqc7^o)fse%r)0a+q7x"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
